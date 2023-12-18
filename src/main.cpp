dba37e93ac1d1fdb070087fba525dca88d939fd4
Lovro Posarić <lovro.posaric@gmail.com>
2023-12-18 09:32:21 +0100

    Actually init an "empty" project

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,226 @@
 #include <Arduino.h>
+#include <AzIoTSasToken.h>
+#include <SerialLogger.h>
+#include <WiFi.h>
+#include <az_core.h>
+#include <azure_ca.h>
+#include <ctime>
+#include "WiFiClientSecure.h"
 
-void setup() {
-	
+#include "ArduinoJson.h"
+#include "DHTesp.h"
+#include "PubSubClient.h"
+
+/* Azure auth data */
+
+#define HOST "iot-hub-lposaric.azure-devices.net"		 //[Azure IoT host name].azure-devices.net
+#define DEVICE_KEY "X4f3z596Sit3i7e6SL2kb4EFnrQlTJ2HrAIoTJclRy8="	 // Azure Primary key for device
+#define TOKEN_DURATION 60
+
+/* MQTT data for IoT Hub connection */
+const char* mqttBrokerURI = HOST;  // MQTT host = IoT Hub link
+const int mqttPort = AZ_IOT_DEFAULT_MQTT_CONNECT_PORT;	// Secure MQTT port
+const char* mqttC2DTopic = AZ_IOT_HUB_CLIENT_C2D_SUBSCRIBE_TOPIC;	// Topic where we can receive cloud to device messages
+
+// These three are just buffers - actual clientID/username/password is generated
+// using the SDK functions in initIoTHub()
+char mqttClientId[128];
+char mqttUsername[128];
+char mqttPassword[200];
+char publishTopic[200];
+
+/* Auth token requirements */
+
+uint8_t sasSignatureBuffer[256];  // Make sure it's of correct size, it will just freeze otherwise :/
+
+az_iot_hub_client client;
+AzIoTSasToken sasToken(
+	&client, AZ_SPAN_FROM_STR(DEVICE_KEY),
+	AZ_SPAN_FROM_BUFFER(sasSignatureBuffer),
+	AZ_SPAN_FROM_BUFFER(
+		mqttPassword));	 // Authentication token for our specific device
+
+const char* deviceId = "Dev001";  // Device ID as specified in the list of devices on IoT Hub
+
+/* Pin definitions and library instance(s) */
+const int lightPin = 35;
+const int dhtPin = 23; // It used to be tied to 21, but 21 and 22 are default pins for I2C communication (for OLED screens eg.), so we will leave them unused  
+
+const int buttonHappyPin = 2;
+const int buttonSadPin = 4;
+const int buttonNeutralPin = 5;
+char lastSentimentStatus = 'N';
+char neutralStatus = 'N', happyStatus = 'H', sadStatus = 'S';
+
+DHTesp dht;
+
+/* WiFi things */
+
+WiFiClientSecure wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+//const char* ssid = "Lab7";
+//const char* pass = "paDijo-o32ijaco";
+const char* ssid = "TheLabIOT";
+const char* pass = "Yaay!ICanTalkNow";
+short timeoutCounter = 0;
+
+void setupWiFi() {
+	Logger.Info("Connecting to WiFi");
+
+	wifiClient.setCACert((const char*)ca_pem); // We are using TLS to secure the connection, therefore we need to supply a certificate (in the SDK)
+
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, pass);
+
+	while (WiFi.status() != WL_CONNECTED) { // Wait until we connect...
+		Serial.print(".");
+		delay(500);
+
+		timeoutCounter++;
+		if (timeoutCounter >= 20) ESP.restart(); // Or restart if we waited for too long, not much else can you do
+	}
+
+	Logger.Info("WiFi connected");
 }
 
-void loop() {
+void IRAM_ATTR buttonISR(void* status) { // We will use a single ISR to handle all three buttons and will change the current status depending on the argument that's passed in
+	char* statusPtr = (char*)status;  // Convert void pointer to a char pointer
+	char statusValue = *statusPtr;	 // Get the value that the pointer points to
 
+	lastSentimentStatus = statusValue;
+}
+
+void setupButtonInterrupts() { // We will be setting up all the buttons in PULLUP state (so that their default state will be HIGH) and then attaching a FALLING interrupt when they are actually clicked
+	pinMode(buttonHappyPin, INPUT_PULLUP);
+	pinMode(buttonSadPin, INPUT_PULLUP);
+	pinMode(buttonNeutralPin, INPUT_PULLUP);
+
+	attachInterruptArg(buttonHappyPin, buttonISR, &happyStatus, FALLING); // Each button gets attached to the same ISR (fired when the voltage starts falling = button is pressed)
+	attachInterruptArg(buttonSadPin, buttonISR, &sadStatus, FALLING); // A char argument is passed to the ISR ('S', 'N' or 'H') 
+  attachInterruptArg(buttonNeutralPin, buttonISR, &neutralStatus, FALLING);
+}
+
+// Use pool pool.ntp.org to get the current time
+// Define a date on 1.1.2023. and wait until the current time has the same year (by default it's 1.1.1970.)
+void initializeTime() {	 // MANDATORY or SAS tokens won't generate
+  
+}
+
+void setupDHTSensor() { // NOTE: change to DHT11 if you are using the blue temp/humidity sensor
+  dht.setup(dhtPin, DHTesp::DHT22);
+}
+
+void setupLightSensor() {
+  	pinMode(lightPin, INPUT);
+}
+
+// MQTT is a publish-subscribe based, therefore a callback function is called whenever something is published on a topic that device is subscribed to
+// It's also a byte-safe protocol, therefore instead of transfering text, bytes are transfered and they aren't null terminated - so we need ot add \0 to terminate the string
+void callback(char *topic, byte *payload, unsigned int length) { 
+  
+}
+
+bool connectMQTT() {
+
+}
+
+void mqttReconnect() {
+
+}
+
+int getLightValue() { // Get the light value by reading the voltage on the light pin using analogRead()
+  int lightSensorPin = lightPin;
+  int lightValue = analogRead(lightSensorPin); // More light = lower voltage = lower value
+  return map(lightValue, 0, 4096, 100, 0); // analogRead() returns values in range 0-4096, therefore we are scalling to 0-100
+}
+
+String getTelemetryData() { // Get the data and pack it in a JSON message
+  StaticJsonDocument<128> doc; // Create a JSON document we'll reuse to serialize our data into JSON
+  String output = "";
+
+	doc["Sentiment"]["Status"] = (String)lastSentimentStatus;
+
+	JsonObject Ambient = doc.createNestedObject("Ambient");
+	Ambient["Temperature"] = dht.getTemperature();
+	Ambient["Humidity"] = dht.getHumidity();
+	Ambient["Light"] = getLightValue();
+
+	doc["DeviceID"] = (String)deviceId;
+
+	serializeJson(doc, output);
+
+	Logger.Info(output);
+  return output;
+}
+
+void sendTelemetryData() {
+  
+}
+
+long lastTime, currentTime = 0;
+int interval = 5000;
+void checkTelemetry() { // Do not block using delay(), instead check if enough time has passed between two calls using millis() 
+
+}
+
+void sendTestMessageToIoTHub() {
+  az_result res = az_iot_hub_client_telemetry_get_publish_topic(&client, NULL, publishTopic, 200, NULL ); // The receive topic isn't hardcoded and depends on chosen properties, therefore we need to use az_iot_hub_client_telemetry_get_publish_topic()
+  Logger.Info(String(publishTopic));
+  
+  mqttClient.publish(publishTopic, deviceId); // Use https://github.com/Azure/azure-iot-explorer/releases to read the telemetry
+}
+
+bool initIoTHub() {
+  az_iot_hub_client_options options = az_iot_hub_client_options_default(); // Get a default instance of IoT Hub client options
+
+  if (az_result_failed(az_iot_hub_client_init( // Create an instnace of IoT Hub client for our IoT Hub's host and the current device
+          &client,
+          az_span_create((unsigned char *)HOST, strlen(HOST)),
+          az_span_create((unsigned char *)deviceId, strlen(deviceId)),
+          &options)))
+  {
+    Logger.Error("Failed initializing Azure IoT Hub client");
+    return false;
+  }
+
+  size_t client_id_length;
+  if (az_result_failed(az_iot_hub_client_get_client_id(
+          &client, mqttClientId, sizeof(mqttClientId) - 1, &client_id_length))) // Get the actual client ID (not our internal ID) for the device
+  {
+    Logger.Error("Failed getting client id");
+    return false;
+  }
+
+  size_t mqttUsernameSize;
+  if (az_result_failed(az_iot_hub_client_get_user_name(
+          &client, mqttUsername, sizeof(mqttUsername), &mqttUsernameSize))) // Get the MQTT username for our device
+  {
+    Logger.Error("Failed to get MQTT username ");
+    return false;
+  }
+
+  Logger.Info("Great success");
+  Logger.Info("Client ID: " + String(mqttClientId));
+  Logger.Info("Username: " + String(mqttUsername));
+  Logger.Info("Password: " + (String)mqttPassword);
+
+  return true;
+}
+
+void setup() {
+  setupWiFi();
+  initializeTime();
+
+  setupLightSensor();
+	setupDHTSensor();
+	setupButtonInterrupts();
+
+  Logger.Info("Setup done");
+}
+
+
+void loop() { // No blocking in the loop, constantly check if we are connected and gather the data if necessary
+
+  checkTelemetry();
 }
